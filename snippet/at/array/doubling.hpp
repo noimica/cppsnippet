69d4b9a8358bb76a5f842189b110f7fee3153e4d
noimica <noimica.l@gmail.com>
2021-06-19 23:01:33 +0900

    インデントの修正

--- snippet/at/array/doubling.hpp
+++ snippet/at/array/doubling.hpp
@@ -1,34 +1,29 @@
 template <class T>
-struct Doubling
-{
-  const int LOG;
-  vector< vector< int > > table;
-  
-  Doubling(int sz, int64_t lim_t) : LOG(64 - __builtin_clzll(lim_t))
-  {
-    table.assign(LOG, vector< int >(sz, -1));
-  }
-
-  void set_next(int k, int x)
-  {
-    table[0][k] = x;
-  }
-
-  void build()
-  {
-    for(int k = 0; k + 1 < LOG; k++) {
-      for(int i = 0; i < table[k].size(); i++) {
-        if(table[k][i] == -1) table[k + 1][i] = -1;
-        else table[k + 1][i] = table[k][table[k][i]];
-      }
+struct Doubling{
+    const int LOG;
+    vector<vector<int>> table;
+    
+    Doubling(int sz, int64_t lim_t) : LOG(64 - __builtin_clzll(lim_t)){
+        table.assign(LOG, vector<int>(sz, -1));
     }
-  }
 
-  int query(int k, int64_t t)
-  {
-    for(int i = LOG - 1; i >= 0; i--) {
-      if((t >> i) & 1) k = table[i][k];
+    void set_next(int k, int x){
+        table[0][k] = x;
     }
-    return k;
-  }
+
+    void build(){
+        for(int k = 0; k + 1 < LOG; k++) {
+            for(int i = 0; i < table[k].size(); i++) {
+                if(table[k][i] == -1) table[k + 1][i] = -1;
+                else table[k + 1][i] = table[k][table[k][i]];
+            }
+        }
+    }
+
+    int query(int k, int64_t t){
+        for(int i = LOG - 1; i >= 0; i--) {
+            if((t >> i) & 1) k = table[i][k];
+        }
+        return k;
+    }
 };
